07330cef9c301a5a748fa7143e7c2bbb5c55ec2e
WangZhenyu <hiiwzy>
2021-07-14 23:30:30 +0800

    add full screen

--- src/liquid.tsx
+++ src/liquid.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { LiquidChart, LiquidChartProps } from "@opd/g2plot-react";
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 
 const config: LiquidChartProps = {
     padding: 0,
     percent: 0.88,
     autoFit: true
+    
 }
 
 export default () => (
-    <section>
-        <LiquidChart {...config} />
+    <section style = {{height:"100vh"}}>
+        <LiquidChart {...config} style = {{height:"100vh"}}/>
     </section>
-
   );
